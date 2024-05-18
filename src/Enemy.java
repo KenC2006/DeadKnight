a8cd8643e4de60f81e5ab83b203b5839977b6064
joeiscoolpro <162076036+joeiscoolpro@users.noreply.github.com>
2024-05-18 13:33:50 -0400

    d

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,22 +1,12 @@
 public class Enemy extends Character {
 
-    private int health;
     private int phase;
     private double sightRadius;
 
     public Enemy(double x, double y, double width, double height, int health, double sightRadius) {
-        super(x, y, width, height);
-        this.health = health;
+        super(x, y, width, height, health);
         this.sightRadius = sightRadius;
 
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void updateHealth(int newHealth) {
-        health = newHealth;
     }
 
     public int getPhase() {
