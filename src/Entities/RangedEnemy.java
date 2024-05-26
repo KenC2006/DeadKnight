82a52f63c5905d1e27ff6dbbdd340f91e6e5f3f4
joepeewee <beselflan@gmail.com>
2024-05-26 16:09:53 -0700

    updated to main

---
+++ src/Entities/RangedEnemy.java
@@ -0,0 +1,20 @@
+package Entities;
+
+public class RangedEnemy extends Enemy {
+    public RangedEnemy(double x, double y, int health) {
+        super(x, y, 2, 5, health, 10);
+
+    }
+
+    public void shoot() {
+
+    }
+
+    public void updateData() {
+
+    }
+
+    public void updateValues() {
+
+    }
+}
