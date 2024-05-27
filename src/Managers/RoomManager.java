475f7f552f814f4e3467a1ad70118910fff0b7d5
joepeewee <beselflan@gmail.com>
2024-05-27 12:04:26 -0700

    fixed passing player into enemy

--- src/Managers/RoomManager.java
+++ src/Managers/RoomManager.java
@@ -12,6 +12,7 @@ import java.util.*;
 
 public class RoomManager {
     private final ArrayList<Room> allPossibleRooms = new ArrayList<>();
+//    private NodeMap nodeMap;
     private ArrayList<Room> allRooms = new ArrayList<>();
     private ArrayList<Room> loadedRooms = new ArrayList<>();
     private Deque<Room> toGenerateNeighbours = new ArrayDeque<>();
@@ -20,6 +21,7 @@ public class RoomManager {
 //        createRectangleRoom();
         loadRoomsFromFile();
         generateRooms();
+//        nodeMap = new NodeMap(allRooms);
     }
 
     public void createRectangleRoom() {
@@ -32,6 +34,7 @@ public class RoomManager {
         for (Room room : loadedRooms) {
             room.drawRoom(c);
         }
+//        nodeMap.drawNodes(c);
     }
 
     public ArrayList<Room> getLoadedRooms() {
