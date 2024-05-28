d762b069a517cd6c6786e36ab05e2421ea2123a8
joepeewee <beselflan@gmail.com>
2024-05-27 17:30:34 -0700

    added beta node generation

--- src/Structure/Vector2F.java
+++ src/Structure/Vector2F.java
@@ -1,7 +1,9 @@
 package Structure;
 
-public class Vector2F {
-    private double x = 0, y = 0;
+import java.util.Comparator;
+
+public class Vector2F implements Comparable<Vector2F> {
+    private double x, y;
 
     public Vector2F() {
 
@@ -117,6 +119,7 @@ public class Vector2F {
         return new Vector2F(-getX(), -getY());
     }
 
+    @Override
     public int compareTo(Vector2F other) {
         if (other.getX() == getX() && other.getY() == getY()) return 0;
         if (other.getX() < getX()) return 1;
